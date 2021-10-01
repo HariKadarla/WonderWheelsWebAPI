3226d6c3703b6916382f3f4041c08baa29d41739
Hari Kishore <harikishorekadarla@gmail.com>
2021-10-01 14:53:02 +0530

    Hari Kishore__ Added BusFetchController and RouteFetchController

---
+++ WonderWheelsWebAPI/Controllers/BusesFetchController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//Added...
+
+using WonderWheelsWebAPI.Model;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace WonderWheelsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BusesFetchController : ControllerBase
+    {
+        public IConfiguration _configuration;
+
+        private readonly BusReservationContext _context;
+
+        public BusesFetchController(BusReservationContext context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+
+        public async Task<ActionResult<IEnumerable<BusDetail>>> Post(BusDetail _bus)
+        {
+            if (_bus != null && _bus.RouteId != 0 && _bus.DepartureDate != null)
+            {
+                List<BusDetail> buses = await _context.BusDetails.Where(b => b.RouteId == _bus.RouteId && b.DepartureDate == _bus.DepartureDate).ToListAsync();
+
+
+                if (buses != null)
+                {
+                    return buses;
+                }
+                else
+                {
+                    return BadRequest("No Route Found");
+                }
+            }
+            else
+            {
+                return BadRequest("Input Source and Destination");
+            }
+        }
+        /*
+        private async Task<List<BusDetail>> GetBuses(int routeid, DateTime? departuredate)
+        {
+             return  (List<BusDetail>) _context.BusDetails.ToList().Where(u => u.RouteId == routeid && u.DepartureDate == departuredate);
+        } */
+    }
+}
---
+++ WonderWheelsWebAPI/Controllers/RouteFetchController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//Added...
+
+using WonderWheelsWebAPI.Model;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace WonderWheelsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RouteFetchController : ControllerBase
+    {
+        public IConfiguration _configuration;
+
+        private readonly BusReservationContext _context;
+
+        public RouteFetchController(BusReservationContext context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+
+        public async Task<ActionResult<Route>> Post(Route _route)
+        {
+            if (_route != null && _route.Source != null && _route.Destination != null)
+            {
+                Route routeDetails = await GetRoute(_route.Source, _route.Destination);
+
+                if (routeDetails != null)
+                {
+                    return routeDetails;
+                }
+                else
+                {
+                    return BadRequest("No Route Found");
+                }
+            }
+            else
+            {
+                return BadRequest("Input Source and Destination");
+            }
+        }
+        private async Task<Route> GetRoute(string source, string destination)
+        {
+            return await _context.Routes.FirstOrDefaultAsync(u => u.Source == source && u.Destination == destination);
+        }
+    }
+}
